3aae57dfa42fcb4e6e24545b5bbcf696f3a42c86
Catriona Barker <catriona.barker@wandisco.com>
2020-06-22 18:12:54 +0100

    restructure

---
+++ website/sidebars.js
@@ -0,0 +1,116 @@
+module.exports = {
+"docs": {
+    "docs.wandisco.com": [
+        "docs/doc1"
+    ],
+    "Architecture": [
+        "docs/architecture/arch_adlsg1_adlsg2",
+        "docs/architecture/arch_hdp_sandbox_adlsg2",
+        "docs/architecture/arch_hdp_sandbox_az_databricks_lan"
+    ],
+    "Network": [
+        "docs/network/net_bandwidth_considerations"
+    ],
+    "Configuration properties": [
+        "docs/config-properties/config-properties",
+        "docs/config-properties/application.properties",
+        "docs/config-properties/ihc.server"
+    ]
+},
+"Why Fusion?": {
+    "Product Concepts": [
+        "why-fusion/benefits",
+        "why-fusion/features",
+        "why-fusion/deployment_models",
+        "why-fusion/core_platform_and_dcone",
+        "why-fusion/concepts_replication_rules",
+        "why-fusion/concepts_consistency_checks",
+        "why-fusion/migration_replication",
+        "why-fusion/hive_replication"
+    ]
+},
+"api": {
+    "API": [
+        "api/api"
+    ]
+},
+"help": {
+    "Help": [
+        "help/need_help",
+        "help/variables"
+    ]
+},
+"quickstarts": {
+    "Preparation": [
+        "quickstarts/preparation/aws_vm_creation",
+        "quickstarts/preparation/azure_vm_creation",
+        "quickstarts/preparation/vm_prep"
+    ],
+    "Installation": [
+        "quickstarts/installation/quickstart-config",
+        "quickstarts/installation/adlsg1-adlsg2",
+        "quickstarts/installation/cdh_sandbox-adlsg2_lm",
+        "quickstarts/installation/cdh_sandbox-adlsg2_ld",
+        "quickstarts/installation/cdh_sandbox_lhv_client-adlsg2_lan",
+        "quickstarts/installation/cdh_sandbox-s3_lm",
+        "quickstarts/installation/cdh_sandbox-s3_ld",
+        "quickstarts/installation/hdp_sandbox_adlsg2_lm",
+        "quickstarts/installation/hdp_sandbox_adlsg2_ld",
+        "quickstarts/installation/hdp_sandbox_lhv_client-adlsg2_lan",
+        "quickstarts/installation/hdp_sandbox-s3_lm",
+        "quickstarts/installation/hdp_sandbox-s3_ld",
+        "quickstarts/installation/s3-adlsg2_bi_lm"
+    ],
+    "Operation": [
+        "quickstarts/operation/create-rule",
+        "quickstarts/operation/consistency-check",
+        "quickstarts/operation/migration",
+        "quickstarts/operation/license_upgrade",
+        "quickstarts/operation/cdh_sandbox_fusion_stop_start",
+        "quickstarts/operation/hdp_sandbox_fusion_stop_start"
+    ],
+    "Testing": [
+        "quickstarts/testing/test_cdh_sandbox",
+        "quickstarts/testing/test_hdp_sandbox"
+    ],
+    "Troubleshooting": [
+        "quickstarts/troubleshooting/general_troubleshooting",
+        "quickstarts/troubleshooting/cdh_sandbox_troubleshooting",
+        "quickstarts/troubleshooting/hdp_sandbox_troubleshooting",
+        "quickstarts/troubleshooting/adlsg2_troubleshooting",
+        "quickstarts/troubleshooting/databricks_troubleshooting",
+        "quickstarts/troubleshooting/logs",
+        "quickstarts/troubleshooting/useful_info"
+    ]
+},
+"Glossary": {
+    "Glossary": [
+        "glossary/a",
+        "glossary/b",
+        "glossary/c",
+        "glossary/d",
+        "glossary/e",
+        "glossary/f",
+        "glossary/g",
+        "glossary/h",
+        "glossary/i",
+        "glossary/j",
+        "glossary/k",
+        "glossary/l",
+        "glossary/m",
+        "glossary/n",
+        "glossary/o",
+        "glossary/p",
+        "glossary/q",
+        "glossary/r",
+        "glossary/s",
+        "glossary/t",
+        "glossary/u",
+        "glossary/v",
+        "glossary/w",
+        "glossary/x",
+        "glossary/y",
+        "glossary/z"
+    ]
+  },
+};

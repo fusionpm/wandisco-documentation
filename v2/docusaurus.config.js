4d6e8ac78542f692da811de24226cc25372cdaa6
Tayler Smith <tayler.smith@wandisco.com>
2020-06-22 15:52:34 +0100

    Update index.js and footer

--- v2/docusaurus.config.js
+++ v2/docusaurus.config.js
@@ -1,75 +1,106 @@
+const path = require("path");
+
 module.exports = {
-  title: 'WANdisco docs',
-  tagline: 'What\'s new',
-  url: 'https://wandisco.github.io/wandisco-documentation/',
-  baseUrl: '/',
-  favicon: href='img/favicon.png',
-  organizationName: 'WANdisco',
-  projectName: 'wandisco-documentation',
+  title: "WANdisco docs",
+  tagline: "What's new",
+  url: "https://wandisco.github.io/wandisco-documentation/",
+  baseUrl: "/",
+  favicon: "img/favicon.png",
+  organizationName: "WANdisco",
+  projectName: "wandisco-documentation",
   themeConfig: {
     sidebarCollapsible: true,
     navbar: {
-      title: 'WANdisco Fusion',
+      title: "WANdisco Fusion",
       logo: {
-        alt: 'WANdisco Fusion Logo',
-        src: 'img/favicon.png',
+        alt: "WANdisco Fusion Logo",
+        src: "img/favicon.png",
       },
       links: [
-        {to: 'docs/why-fusion/benefits', label: 'Why Fusion?', position: 'right'},
-        {to: 'docs/quickstarts/installation/quickstart-config', label: 'Quickstarts', position: 'right'},
-        {to: 'docs/docs/doc1', label: 'Docs', position: 'right'},
-        {to: 'docs/glossary/a', label: 'Glossary', position: 'right'},
+        {
+          to: "docs/why-fusion/benefits",
+          label: "Why Fusion?",
+          position: "right",
+        },
+        {
+          to: "docs/quickstarts/installation/quickstart-config",
+          label: "Quickstarts",
+          position: "right",
+        },
+        { to: "docs/docs/doc1", label: "Docs", position: "right" },
+        { to: "docs/glossary/a", label: "Glossary", position: "right" },
         //  {doc: 'api/api', label: 'API', position: 'right'},
-        {to: 'docs/help/need_help', label: 'Help', position: 'right'},
+        { to: "docs/help/need_help", label: "Help", position: "right" },
       ],
     },
     algolia: {
-      apiKey: '47ecd3b21be71c5822571b9f59e52544',
-      indexName: 'docusaurus-2',
+      apiKey: "47ecd3b21be71c5822571b9f59e52544",
+      indexName: "docusaurus-2",
       algoliaOptions: {},
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: 'docs/quickstarts/'
+              label: "Getting Started",
+              to: "docs/quickstarts/installation/quickstart-config",
             },
             {
-              label: 'Product User Guides',
-              href: 'https://docs.wandisco.com'
-            }
-          ]
+              label: "Product User Guides",
+              href: "https://docs.wandisco.com",
+            },
+            {
+              label: "Knowledge Base",
+              href: "https://community.wandisco.com/s/knowledge-base",
+            },
+          ],
         },
         {
-          title: 'Community',
-          items: []
+          title: "Community",
+          items: [
+            {
+              label: "WANdisco Community",
+              href: "https://community.wandisco.com/",
+            },
+            {
+              label: "Partners",
+              href: "//wandisco.com/partners/find",
+            },
+          ],
         },
         {
-          title: 'More',
-          links: []
-        }
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              href: "https://wandisco.com/blog",
+            },
+            {
+              label: "Developer Blog",
+              href: "https://blogs.wandisco.com/",
+            },
+          ],
+        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} WANdisco, Inc.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          path: '../docs',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "../docs",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- v2/src/pages/index.js
+++ v2/src/pages/index.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: 'LiveMigrator',
-    imageUrl: 'img/hybrid-cloud.png',
-    description: 'Migrate data to cloud without disruption or downtime.'
+    title: "LiveMigrator",
+    imageUrl: "img/hybrid-cloud.png",
+    description: "Migrate data to cloud without disruption or downtime.",
   },
   {
-    title: 'LiveAnalytics',
-    imageUrl: 'img/Flexibility.svg',
-    description: 'Immediate analytic data access.'
+    title: "LiveAnalytics",
+    imageUrl: "img/Flexibility.svg",
+    description: "Immediate analytic data access.",
   },
   {
-    title: 'Docker',
-    imageUrl: 'img/docker.png',
-    description: 'Get up and running in minutes with Fusion & Docker.'
+    title: "Docker",
+    imageUrl: "img/docker.png",
+    description: "Get up and running in minutes with Fusion & Docker.",
   },
   {
-    title: 'OneUI',
-    imageUrl: 'img/WD_Oracle_Benefits-03.png',
-    description: 'Manage your whole Fusion ecosystem in one place.'
+    title: "OneUI",
+    imageUrl: "img/WD_Oracle_Benefits-03.png",
+    description: "Manage your whole Fusion ecosystem in one place.",
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--6', styles.feature)}>
+    <div className={clsx("col col--6", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -46,22 +46,34 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title} | ${siteConfig.tagline}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
-              className={clsx(
-                'button button--primary button--lg'
-              )}
-              to={useBaseUrl('docs/')}>
+              className={clsx("button button--primary button--lg")}
+              to={useBaseUrl("docs/quickstarts/installation/quickstart-config")}
+            >
               Get Started
+            </Link>
+            <Link
+              className={clsx("button button--primary button--lg")}
+              to="https://wandisco.tech"
+            >
+              WANdisco.tech
+            </Link>
+            <Link
+              className={clsx("button button--primary button--lg")}
+              to="https://www.docker.com/get-started"
+            >
+              Get Docker
             </Link>
           </div>
         </div>
